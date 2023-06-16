2886103d010b497d9490727cfed137b0e78b111b
hari saputra <hari.saputra.dev@gmail.com>
2023-06-16 14:13:52 +0700

    add factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Company;
+use App\Models\Employee;
+use App\Models\Responsibility;
+use App\Models\Role;
+use App\Models\Team;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +19,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+
+        // Company::factory(10)->create();
+        // Team::factory(30)->create();
+        // Role::factory(10)->create();
+        // Responsibility::factory(40)->create();
+        Employee::factory(150)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
